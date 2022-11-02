a684fb9d95542cb29d0bf9e0326b47d3bbe3ba96
rin2407 <vanrin24071998@gmail.com>
2022-11-02 21:34:14 +0700

    setting i18n, pretier format

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,17 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-
+import { I18nextProvider } from 'react-i18next';
+import i18n from './_i18n/i18n';
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
     </Provider>
   </React.StrictMode>
 );
---
+++ src/locales/en/index.tsx
@@ -0,0 +1,5 @@
+import Home from './home.json'
+const en = {
+    ...Home
+}
+export default en
